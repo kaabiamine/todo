11116ed034b508317b419e9c4cf64b38087bbf6d
kaabiamine <amine.kaabi@esprit.tn>
2025-08-01 09:28:49 +0100

    Fix case sensitivity for routes folder

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
-const todosRoutes = require("./Routes/todosRoutes");
+const todosRoutes = require("./routes/todosRoutes");
 
 app.use(cors());
 app.use(express.json());
